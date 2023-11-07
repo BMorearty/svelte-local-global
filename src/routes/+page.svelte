c70c694cfc592d7996dc688f24dc6350c5168a98
Brian Morearty <brian@morearty.org>
2023-11-07 13:18:45 -0800

    upgrade to svelte 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-  import { slide } from 'svelte/transition';
+  import {slide} from 'svelte/transition';
+
   let show = false;
   let items = [];
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 25; i++) {
     items.push(i);
   }
 </script>
@@ -12,11 +13,14 @@
 <p><a href="other">Visit another page</a></p>
 
 <input type="button" value="toggle" on:click={() => show = !show}>
+<input type="button" value="insert" on:click={() => items = [...items, 'howdy']}>
 
 {#if show}
-  {#each items as item}
-    <div transition:slide|global>
-      {item}
-    </div>
-  {/each}
+  <div transition:slide>
+    {#each items as item}
+      <div transition:slide>
+        {item}
+      </div>
+    {/each}
+  </div>
 {/if}
