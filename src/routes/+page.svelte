004e90b950486fb35b82998985de954f76d18d71
Brian Morearty <brian@morearty.org>
2023-11-07 12:43:48 -0800

    other

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
+<script>
+  import { slide } from 'svelte/transition';
+  let show = false;
+  let items = [];
+  for (let i = 0; i < 100; i++) {
+    items.push(i);
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><a href="other">Visit another page</a></p>
+
+<input type="button" value="toggle" on:click={() => show = !show}>
+
+{#if show}
+  {#each items as item}
+    <div transition:slide>
+      {item}
+    </div>
+  {/each}
+{/if}
